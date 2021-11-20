992966fc7585e7095eb3fc021575fd1b47732a80
Javohir <javohir200903@gmail.com>
2021-11-20 14:04:46 +0500

    Add CreateBanPostController

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -1,7 +1,29 @@
+const { CreateBanValidation } = require("../validations/AdminValidation");
+
 module.exports = class AdminController {
     static async CreateBanPostController(req, res, next) {
         try {
-            //
+            const data = await CreateBanValidation(req.body, res.error);
+
+            const user = await req.db.users.findOne({
+                where: {
+                    user_id: data.user_id,
+                },
+            });
+
+            if (!user) throw new res.error(404, "User not found");
+
+            const ban = await req.db.user_bans.create({
+                user_id: data.user_id,
+                ban_reason: data.ban_reason,
+                ban_expire_date: data.ban_expire_date,
+            });
+
+            res.status(201).json({
+                ok: true,
+                message: "Ban created",
+                data: { ban },
+            });
         } catch (error) {
             next(error);
         }
